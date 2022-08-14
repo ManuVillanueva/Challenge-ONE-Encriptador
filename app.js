61437c2b5e3e9544ba629ee334fd2f86f1f9825f
Manuel Villanueva <manu.bsso@hotmail.es>
2022-08-14 13:54:44 -0300

    Agrego el archivo .js para crear la logica de la pagina

---
+++ app.js
@@ -0,0 +1,19 @@
+/*Este programa tiene que ser capaz de encriptar un texto que ingrese
+el usuario, y a su vez tiene que ser capaz de volver a desencriptarlo*/
+
+/*Reglas que tiene que seguir el codigo
+la letra "e" tiene que ser convertida a "enter";
+la letra "i" tiene que ser convertida a "imes";
+la letra "a" tiene que ser convertida a "ai";
+la letra "o" tiene que ser convertida a "ober";
+la letra "u" tiene que ser convertida a "ufat";
+
+Ademas solo deebe funcionar con letras minúsculas,
+tampoco tienen que tener tilde, ni caracteres especiales*/
+
+var textoIngresado = [];
+
+function encriptar() {
+    
+    
+}
